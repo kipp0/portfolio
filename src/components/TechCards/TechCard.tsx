0ae68e63b53e34ba473f4603cbe1d0568cc53f87
Pierre N. Smith <smith.pierre@me.com>
2022-10-06 17:55:26 -0400

    test

---
+++ src/components/TechCards/TechCard.tsx
@@ -0,0 +1,25 @@
+
+type TechCardProps = {
+    name: string;
+    description: string;
+    documentation: string;
+};
+
+function TechCard( { name, description, documentation }: TechCardProps ) {
+    return (
+        <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
+            <h2 className="text-lg text-gray-700">{name}</h2>
+            <p className="text-sm text-gray-600">{description}</p>
+            <a
+                className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
+                href={documentation}
+                target="_blank"
+                rel="noreferrer"
+            >
+                Documentation
+            </a>
+        </section>
+    );
+}
+
+export default TechCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Header from "../components/Header";
+import TechCard from "../components/TechCards";
 
 const Home: NextPage = () => {
   return (
@@ -21,28 +22,34 @@ const Home: NextPage = () => {
       </Header>
       <main className="container mx-auto flex flex-col items-center justify-center min-h-screen p-4">
 
-        <p className="text-2xl text-gray-700">This stack uses:</p>
+        <p className="text-2xl text-gray-700">My Bread and Butter:</p>
         <div className="grid gap-3 pt-3 mt-3 text-center md:grid-cols-3 lg:w-2/3">
-          <TechnologyCard name="NextJS" description="The React framework for production" documentation="https://nextjs.org/" /> <TechnologyCard name="TypeScript"
-            description="Strongly typed programming language that builds on JavaScript, giving you better tooling at any scale"
-            documentation="https://www.typescriptlang.org/"
+          <TechCard
+            name="NextJS"
+            description=""
+            documentation="https://nextjs.org/"
           />
-          <TechnologyCard
+
+          <TechCard name="TypeScript"
+            description="Started using typescript during a "
+            documentation="Since 2020"
+          />
+          <TechCard
             name="TailwindCSS"
             description="Rapidly build modern websites without ever leaving your HTML"
             documentation="https://tailwindcss.com/"
           />
-          <TechnologyCard
+          <TechCard
             name="tRPC"
             description="End-to-end typesafe APIs made easy"
             documentation="https://trpc.io/"
           />
-          <TechnologyCard
+          <TechCard
             name="Next-Auth"
             description="Authentication for Next.js"
             documentation="https://next-auth.js.org/"
           />
-          <TechnologyCard
+          <TechCard
             name="Prisma"
             description="Build data-driven JavaScript & TypeScript apps in less time"
             documentation="https://www.prisma.io/docs/"
@@ -55,29 +62,3 @@ const Home: NextPage = () => {
 
 export default Home;
 
-type TechnologyCardProps = {
-  name: string;
-  description: string;
-  documentation: string;
-};
-
-const TechnologyCard = ({
-  name,
-  description,
-  documentation,
-}: TechnologyCardProps) => {
-  return (
-    <section className="flex flex-col justify-center p-6 duration-500 border-2 border-gray-500 rounded shadow-xl motion-safe:hover:scale-105">
-      <h2 className="text-lg text-gray-700">{name}</h2>
-      <p className="text-sm text-gray-600">{description}</p>
-      <a
-        className="mt-3 text-sm underline text-violet-500 decoration-dotted underline-offset-2"
-        href={documentation}
-        target="_blank"
-        rel="noreferrer"
-      >
-        Documentation
-      </a>
-    </section>
-  );
-};
