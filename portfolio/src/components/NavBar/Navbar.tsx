00d35fdb64e195542350307a13f748d9160be1a9
Pierre N. Smith <smith.pierre@me.com>
2022-10-20 14:13:40 -0400

    fixing commit

--- portfolio/src/components/NavBar/Navbar.tsx
+++ portfolio/src/components/NavBar/Navbar.tsx
@@ -27,7 +27,7 @@ function Navbar( { navigationLinks }: NavbarProps ) {
                         <div className="relative flex h-16 items-center justify-between">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 <Disclosure.Button
-                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                                    className="inline-flex items-center justify-center rounded-md p-2 text-gray-700 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                                 >
 
                                     <span className="sr-only">Open main menu</span>
--- portfolio/src/pages/projects.tsx
+++ portfolio/src/pages/projects.tsx
@@ -1,8 +1,9 @@
 import type { NextPage } from "next";
+import Image from "next/future/image";
 import Head from "next/head";
 import Footer from "../components/Footer/Footer";
 import Header from "../components/Header";
-
+import me from '../assets/me.jpeg'
 
 const Projects: NextPage = () => {
     return (
@@ -13,7 +14,20 @@ const Projects: NextPage = () => {
             </Head>
             <Header />
             <main className="relative ring ring-zinc-100 inset-0 bg-white max-w-6xl m-auto flex flex-col items-center justify-center min-h-screen p-4 rounded">
-                <p className="text-2xl text-gray-700">Github Projects comming soon..</p>
+                <section className="testimonial-left relative grid grid-rows-[repeat(5,_1fr)] grid-cols-1 w-full h-[700px] pt-14 border rounded border-zinc-100">
+                    <div className="relative row-[1_/_3] col-start-1 overflow-hidden px-5 z-10 rounded">
+                        <Image
+                            src={me}
+                            className={`rounded-xl object-fill`}
+                            alt={`testimonial`}
+                        />
+                    </div>
+                    <div className="row-[2_/_-1] col-start-1 px-5 bg-emerald-500 pt-44">
+                        <p>
+                            Pierre was perfessional and
+                        </p>
+                    </div>
+                </section>
             </main>
             <Footer />
         </>
